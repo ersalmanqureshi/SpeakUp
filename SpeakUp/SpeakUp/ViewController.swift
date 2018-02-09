8b72030db930c9a16686407c3071e29c0e15a50f
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-02-09 17:44:23 +0530

    Implement speech framework

--- SpeakUp/SpeakUp/ViewController.swift
+++ SpeakUp/SpeakUp/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var microPhoneButton: UIButton!
+    
+    var audioEngine: AVAudioEngine!
+    private let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "en_US"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    
+    @IBAction func tapButtonDidTap(_ sender: UIButton) {
+        
+        
+        var isButtonEnabled = false
+        
+        SFSpeechRecognizer.requestAuthorization { (authorizationStatus) in
+            switch authorizationStatus {
+            case .authorized:
+                isButtonEnabled = true
+            case .denied:
+                isButtonEnabled = false
+                print("Permission denied")
+            case .restricted:
+                isButtonEnabled = false
+                print("Permission restricted")
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Permission not determined")
+            }
+        }
+        
+        OperationQueue.main.addOperation {
+            self.microPhoneButton.isEnabled = isButtonEnabled
+        }
+    }
+    
+    func recordAndRecognizeSpeech() {
+      
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        recordAndRecognizeSpeech()
     }
 
     override func didReceiveMemoryWarning() {
