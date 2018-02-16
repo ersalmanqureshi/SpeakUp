928ecc950f2170fce82e69d640084ce1698cc521
Salman Qureshi <er.salmanqureshi@gmail.com>
2018-02-16 19:35:36 +0530

    Speech recognizer delegate and alert to show

--- SpeakUp/SpeakUp/ViewController.swift
+++ SpeakUp/SpeakUp/ViewController.swift
@@ -9,40 +9,36 @@
 import UIKit
 import Speech
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SFSpeechRecognizerDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var microPhoneButton: UIButton!
     
-    private let audioEngine = AVAudioEngine()
+    let audioEngine = AVAudioEngine()
     private let speechRecognizer: SFSpeechRecognizer? = SFSpeechRecognizer(locale: Locale.init(identifier: "en_US"))
     let request = SFSpeechAudioBufferRecognitionRequest()
     var recognitionTask: SFSpeechRecognitionTask?
     
+    var isRecording = false
+    
     @IBAction func tapButtonDidTap(_ sender: UIButton) {
-        
-        recordAndRecognizeSpeech()
-//        var isButtonEnabled = false
-//
-//        SFSpeechRecognizer.requestAuthorization { (authorizationStatus) in
-//            switch authorizationStatus {
-//            case .authorized:
-//                isButtonEnabled = true
-//                self.recordAndRecognizeSpeech()
-//            case .denied:
-//                isButtonEnabled = false
-//                print("Permission denied")
-//            case .restricted:
-//                isButtonEnabled = false
-//                print("Permission restricted")
-//            case .notDetermined:
-//                isButtonEnabled = false
-//                print("Permission not determined")
-//            }
-//        }
-//
-//        OperationQueue.main.addOperation {
-//            self.microPhoneButton.isEnabled = isButtonEnabled
-//        }
+    
+        if isRecording == true {
+            audioEngine.stop()
+            recognitionTask?.cancel()
+            isRecording = false
+            microPhoneButton.backgroundColor = UIColor.gray
+        } else {
+            self.recordAndRecognizeSpeech()
+            isRecording = true
+            microPhoneButton.backgroundColor = UIColor.red
+        }
+    }
+    
+    func cancelRecording() {
+        audioEngine.stop()
+        let node = audioEngine.inputNode
+            node.removeTap(onBus: 0)
+        recognitionTask?.cancel()
     }
     
     func recordAndRecognizeSpeech() {
@@ -57,14 +53,17 @@ class ViewController: UIViewController {
         do {
             try audioEngine.start()
         } catch {
+             self.showAlert(message: "There has been an audio engine error.")
             print("Error")
         }
         
         guard let myRecognizer = SFSpeechRecognizer() else {
+            self.showAlert(message: "Speech recognition is not supported for your current locale.")
             return
         }
         
         if !myRecognizer.isAvailable {
+            self.showAlert(message: "Speech recognition is not currently available.")
             return
         }
         
@@ -73,7 +72,7 @@ class ViewController: UIViewController {
                 let bestString = result.bestTranscription.formattedString
                 self.textField.text = bestString
             } else if let error = error{
-                print(error)
+                self.showAlert(message: "There has been a speech recognition error.")
             }
         })
         
@@ -81,7 +80,27 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        recordAndRecognizeSpeech()
+        requestSpeechAuthorization()
+    }
+    
+    func requestSpeechAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.microPhoneButton.isEnabled = true
+                case .denied:
+                    self.microPhoneButton.isEnabled = false
+                   // self.detectedTextLabel.text = "User denied access to speech recognition"
+                case .restricted:
+                    self.microPhoneButton.isEnabled = false
+                   // self.detectedTextLabel.text = "Speech recognition restricted on this device"
+                case .notDetermined:
+                    self.microPhoneButton.isEnabled = false
+                   // self.detectedTextLabel.text = "Speech recognition not yet authorized"
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -89,6 +108,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Speech Recognizer Error", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
